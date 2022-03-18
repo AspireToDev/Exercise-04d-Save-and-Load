ed3de172f1a58f7b579a7ff47b590c9daca847ec
AspireToDev <97562355+AspireToDev@users.noreply.github.com>
2022-03-18 13:30:12 -0400

    Enemy Collision

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -8,13 +8,15 @@ func _ready():
 func damage(d):
 	health -= d
 	if health <= 0:
-		queue_free()
-		Global.update_score(10)
+		die()
 	print(d)
 
 
 func _on_Area2D_body_entered(body):
-	if body.has_method("damage") and not self:
+	if body.has_method("damage") and body != self:
 		print("Collide")
 		body.damage(30)
-		queue_free()
+		die()
+func die():
+	Global.update_score(10)
+	queue_free()
